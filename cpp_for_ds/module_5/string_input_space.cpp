03c715255e380def162c4f11d881bdcec371e1c9
shubho-saha <shuvo7242@gmail.com>
2024-06-01 16:39:14 +0600

    cpp_ds module5

---
+++ cpp_for_ds/module_5/string_input_space.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s1, s2;
+    int a;
+    getline(cin, s1);
+    cin>>a;
+    cin.ignore();
+    getline(cin, s2);
+
+    cout<<s1<<endl;
+    cout<<a<<endl;
+    cout<<s2<<endl;
+   
+
+    return 0;
+}
