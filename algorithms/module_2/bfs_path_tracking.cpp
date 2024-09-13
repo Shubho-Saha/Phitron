4507c1bdf55b904213901147c5025aaef35f475a
shubho-saha <shuvo7242@gmail.com>
2024-09-13 10:09:38 +0600

    algo module 2

---
+++ algorithms/module_2/bfs_path_tracking.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> adlist[100];
+int visited[100];
+int parent[100];
+int level[100];
+
+void bfs(int src)
+{
+    queue<int> q;
+    q.push(src);
+    visited[src] = true;
+    level[src] = 0;
+
+    while(!q.empty())
+    {
+        int root = q.front();
+        q.pop();
+
+        for(int child: adlist[root]){
+            if (!visited[child]) {
+                q.push(child);
+                visited[child] = true;
+                level[child] = level[root] + 1;
+                parent[child] = root;
+            }
+        }
+
+        
+  
+    }
+    
+}
+int main()
+{
+    int n, e;
+    cin>>n>>e;
+
+    while(e--)
+    {
+        int a, b;
+        cin>>a>>b;
+        adlist[a].push_back(b);
+        adlist[b].push_back(a);
+    }
+
+    memset(visited, false, sizeof(visited));
+    memset(parent, -1, sizeof(parent));
+    memset(level, -1, sizeof(level));
+    int src, des;
+    cin>>src>>des;
+    bfs(src);
+    while(des != -1) {
+        cout<<des<<" ";
+        des = parent[des];
+    }
+
+
+    return 0;
+}
---
+++ algorithms/module_2/bfs_with_level_pair.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> adlist[100];
+int visited[100];
+
+void bfs(int src, int des)
+{
+    queue<pair<int, int>> q;
+    q.push({src, 0});
+    visited[src] = true;
+    bool flag = false;
+    while(!q.empty())
+    {
+        pair<int, int> p = q.front();
+        q.pop();
+        int element = p.first;
+        int level = p.second;
+        if (element == des) {
+            cout<<level<<endl;
+            flag = true;
+        }
+
+        for(int child : adlist[element]) {
+            if (!visited[child]) {
+                q.push({child, level+1});
+                visited[child] = true;
+            }
+        }
+    }
+    if (!flag) {
+        cout<<"Not found"<<endl;
+    }
+}
+
+int main()
+{
+    int n, e;
+    cin>>n>>e;
+
+    while(e--)
+    {
+        int a, b;
+        cin>>a>>b;
+        adlist[a].push_back(b);
+        adlist[b].push_back(a);
+    }
+
+    memset(visited, false, sizeof(visited));
+    int src;
+    cin>>src;
+    int des;
+    cin>>des;
+    bfs(src, des);
+
+    return 0;
+}
